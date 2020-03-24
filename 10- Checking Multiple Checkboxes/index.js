3dfcee2949d464f26b6442694418bb13988c8def
yagmurcetintas <yagmurcetin@gmail.com>
2020-03-24 12:54:16 -0400

    added selecting multiple checkboxes logic

---
+++ 10- Checking Multiple Checkboxes/index.js
@@ -0,0 +1,43 @@
+const listItems = document.querySelectorAll('.item');
+const checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+
+let shiftDown = false;
+let lastCheckbox = null;
+
+console.log(checkboxes);
+
+checkboxes.forEach(currentCheckbox => currentCheckbox.addEventListener('click', event => {
+  if (shiftDown) {
+    if (lastCheckbox) {
+
+      let checkboxesToTick = [];
+
+      const indexOfLastCheckbox = checkboxes.indexOf(lastCheckbox);
+      const indexOfCurrentCheckbox = checkboxes.indexOf(currentCheckbox);
+
+      if (indexOfCurrentCheckbox <= indexOfLastCheckbox) {
+        checkboxesToTick = checkboxes.slice(indexOfCurrentCheckbox, indexOfLastCheckbox + 1);
+      } else {
+        checkboxesToTick = checkboxes.slice(indexOfLastCheckbox, indexOfCurrentCheckbox + 1);
+      }
+
+      checkboxesToTick.forEach(checkbox => {
+        checkbox.setAttribute("checked", true);
+      });
+    }
+  }
+
+  lastCheckbox = currentCheckbox;
+}));
+
+window.addEventListener('keydown', event => {
+  if (event.key === "Shift") {
+    shiftDown = true;
+  }
+});
+
+window.addEventListener('keyup', event => {
+  if (event.key === "Shift") {
+    shiftDown = false;
+  }
+});
