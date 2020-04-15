413bcaae7265fab4a63bb7b7b381edddd8e035cb
yagmurcetintas <yagmurcetin@gmail.com>
2020-04-15 12:05:11 -0400

    added the logic of real time video streaming from webcam

--- 19- Webcam Fun/scripts.js
+++ 19- Webcam Fun/scripts.js
@@ -8,5 +8,11 @@ function getVideo() {
   navigator.mediaDevices.getUserMedia({
     video: true,
     audio: false
+  }).then(localMediaStream => {
+    console.log(localMediaStream);
+    video.srcObject = localMediaStream;
+    video.play();
   });
 }
+
+getVideo();
